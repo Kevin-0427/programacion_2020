2ac0aa23ab58283224cba8a928c16c63cb90fcea
Kevin-0427 <68254013+Kevin-0427@users.noreply.github.com>
2020-07-17 16:22:21 -0500

    Update cuota.cs

--- cuota.cs
+++ cuota.cs
@@ -11,17 +11,18 @@ namespace cuota_moderadora
 
             Console.WriteLine("ingrese el valor de su salario");
             double salario = double.Parse(Console.ReadLine());
-
-            if (salario < 1755604)
+            double smmlv = 877803;
+            
+            if (salario < (smmlv*2))
             {
                 Console.WriteLine("La tarifa correspondiente a su pago es la tarifa A"+ "\n"+"el valor de la cuota moderadora es de : $ 3.400 COP");
                 
             }
-            else if (salario >= 1755604 && salario < 4389010)
+            else if (salario >= (smmlv*2) && salario < (smmlv*5))
             {
                 Console.WriteLine("La tarifa correspondiente a su pago es la tarifa B" + "\n" + "el valor de la cuota moderadora es de : $ 13.500 COP");
             }
-            else if (salario >= 4389010)
+            else if (salario >= (smmlv*5))
             {
                 Console.WriteLine("La tarifa correspondiente a su pago es la tarifa C" + "\n" + "el valor de la cuota moderadora es de : $ 35.600 COP");
             }

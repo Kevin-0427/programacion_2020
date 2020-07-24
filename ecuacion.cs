de5e9986aef6ed7fc71269f9e551842858b95913
Kevin-0427 <68254013+Kevin-0427@users.noreply.github.com>
2020-07-24 16:42:11 -0500

    Update ecuacion.cs

--- ecuacion.cs
+++ ecuacion.cs
@@ -27,8 +27,8 @@ namespace ecuacion
                 else
                 {
                     Console.WriteLine("Existen dos posibles soluciones correctas");
-                    x1 = (-b + (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
-                    x2 = (-b - (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
+                    x1 = (-b + (Math.Sqrt(disc)) / (2 * a);
+                    x2 = (-b - (Math.Sqrt(disc)) / (2 * a);
                     Console.WriteLine("soluciones para la ecuación: " + "\n" + " x1 = " + x1 + "\n" + " x2 = " + x2);
 
                 }
@@ -36,7 +36,7 @@ namespace ecuacion
             else
             {
                 Console.WriteLine("la solución existe y es unica");
-                x1 = (-b + (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
+                x1 = (-b + (Math.Sqrt(disc)) / (2 * a);
                 Console.WriteLine("soluciones para la ecuación: " + "\n" + " x1 = " + x1);
             }
             

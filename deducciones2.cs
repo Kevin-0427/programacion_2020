ed1489b48e1f3eb45d245e69812e8502ef8b8c58
Kevin-0427 <68254013+Kevin-0427@users.noreply.github.com>
2020-07-24 15:40:14 -0500

    Add files via upload

---
+++ deducciones2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace deducciones2
+{
+    class deducciones2
+    {
+        static void Main(string[] args)
+        {
+            double pension = 0;
+            double arl = 0;
+            double eps = 0;
+            double bonif = 0;
+            double smmlv = 877803;
+            double sreal = 0;
+            double sanual = 0;
+
+            Console.WriteLine("ingrese el valor de su salario");
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite la opción que corresponde a su Tipo de Contrato" + "\n" + "1. Dependiente" + "\n" + "2. Independiente" + "\n");
+            int contrato = Convert.ToInt32(Console.ReadLine());
+
+            double bcoti = salario * 0.4;
+
+            if (bcoti < smmlv) bcoti = smmlv;
+
+            switch (contrato)
+            {
+                case 1:
+                    bonif = salario;
+                    pension = bcoti * 0.04;
+                    eps = bcoti * 0.04;
+                    double deducciones = pension + eps;
+                    sreal = salario - deducciones;
+                    sanual = (sreal * 12) + bonif;
+
+                    Console.WriteLine("Deducciones: "+deducciones + "\n" + "pensión :" + pension + "    Eps: " + eps);
+                    Console.WriteLine("Su Salario real mensual es: " + sreal + "\n" + "su salario anual es: " + sanual);
+                    break;
+
+                case 2:
+                    Console.WriteLine("digite el nivel de riesgo laboral de 1 a 5");
+                    int riesgo = Convert.ToInt32(Console.ReadLine());
+
+                    switch (riesgo)
+                    {
+                        case 1: arl = 0.00522 * bcoti;
+                            break;
+                        case 2: arl = 0.01044 * bcoti;
+                            break;
+                        case 3: arl = 0.02436 * bcoti;
+                            break;
+                        case 4: arl = 0.04350 * bcoti;
+                            break;
+                        case 5: arl = 0.06960 * bcoti;
+                            break;
+                    }
+                    
+                    pension = bcoti * 0.16;
+                    eps = bcoti * 0.125;
+                    double deduccionesi = pension + eps + arl;
+                    sreal = salario - (pension+eps+arl);
+                    sanual = sreal * 12;
+
+                    Console.WriteLine("Deducciones: " + deduccionesi + "\n" + "pensión :" + pension + "    Eps: " + eps+"    Arl: "+arl);
+                    Console.WriteLine("Su Salario real mensual es: " + sreal + "\n" + "su salario anual es: " + sanual);
+
+                    break;
+            }
+        }
+    }
+}
---
+++ votaciones.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace votaciones
+{
+    class votaciones
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese numero de  votos por el partido 1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese numero de  votos por el partido 2");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese numero de  votos en blanco");
+            int blancos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese numero de  votos anulados");
+            int anulados = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese numero total de la población");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese porcentaje de la población que es mayor de edad");
+            double p = Convert.ToInt32(Console.ReadLine());
+
+            double pme = n * (p / 100);
+            int votantes = a + b + blancos + anulados;
+            double abstencion = pme - votantes;
+
+            bool c1 = anulados < ((a + b) * 0.3);
+            bool c2 = (a + b) > blancos;
+            bool c3 = abstencion < (a + b + blancos + anulados);
+
+            Console.WriteLine("votos: " + "\n" + "partido1: " + a + "  partido2: " + b + "   blancos: " + blancos + "   anulados: " + anulados
+                               + "\n"+"total habitantes: "+n+"   mayores de edad: "+p+"%" + "\n"+"poblacion mayor de edad: "+(int)pme+"\n"
+                               +"total de votantes: "+votantes+"   abstencion: "+(int)abstencion);
+            
+            
+            
+            if(c3 && (c1 || c2))
+            {
+                Console.WriteLine("\n"+"Las votaciones fueron exitosas");
+                if(a > b)
+                {
+                    Console.WriteLine("\n" + "Ganador de las elecciones: Partido 1");
+                }
+                else
+                {
+                    Console.WriteLine("\n" + "Ganador de las elecciones: Partido 2");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("\n" + "Las votaciones deben repetirse");
+            }
+
+
+        }
+
+    }
+}
